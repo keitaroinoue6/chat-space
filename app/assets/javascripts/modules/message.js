6ad765cf04c7a1aaee23a0fbac9366c24b113340
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2020-09-08 17:16:57 +0900

    自動更新機能終了

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="messagebox">
+        `<div class="messagebox" data-message-id=${message.id}>
           <div class="messageinfo">
             <div class="messageinfo__username">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="messagebox">
+      `<div class="messagebox" data-message-id=${message.id}>
         <div class="messageinfo">
           <div class="messageinfo__username">
             ${message.user_name}
@@ -63,5 +63,4 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   });
-  return false;
 });
