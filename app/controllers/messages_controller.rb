5884de09766ecdd232fc7faa50d5e6af92ef9288
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-02-16 17:50:09 +0900

    i

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -15,7 +15,7 @@ class MessagesController < ApplicationController
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
-      render :index     
+      render :index
     end
   end
 
